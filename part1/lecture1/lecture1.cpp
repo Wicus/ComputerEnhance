b1cafe7efa1842e9a9aeca52f08d16e1d0efed5d
Wicus <wicus@mecalc.co.za>
2024-01-20 13:25:43 +0200

    lecture 1 complete

--- part1/lecture1/lecture1.cpp
+++ part1/lecture1/lecture1.cpp
@@ -1,90 +1,133 @@
+#include <cstdint>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct {
-  uint8_t instruction;
-  uint8_t d;
-  uint8_t w;
-  uint8_t mod;
-  uint8_t reg;
-  uint8_t rm;
-} MovInstruction;
+#define OK 0
+#define ERROR 1
 
-void decode(uint8_t firstByte, uint8_t secondByte, char *result, size_t resultSize);
+int32_t Decode(uint8_t firstByte, uint8_t secondByte, char *result, size_t resultSize);
 
-const char *registerFieldsWide[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
-const char *registerFieldsLow[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
-
-int main(int argc, char *argv[]) {
+int32_t main(int32_t argc, char *argv[]) {
   if (argc < 2) {
     printf("Usage: %s <filename>\n", argv[0]);
-    return 1;
+    return ERROR;
   }
 
-  // Open the file for reading
-  FILE *file;
-  errno_t err = fopen_s(&file, argv[1], "rb");
-  if (file == NULL) {
+  FILE *inputFile;
+  errno_t err = fopen_s(&inputFile, argv[1], "rb");
+  if (err != 0) {
     printf("Error opening file: %s\n", argv[1]);
-    return 1;
+    return ERROR;
   }
 
-  // Open the file for writing
-  FILE* outFile;
-  fopen_s(&outFile, "out.asm", "w");
-  if (outFile == NULL) {
+  FILE* outputFile;
+  err = fopen_s(&outputFile, "output.asm", "w");
+  if (err != 0) {
     printf("Error opening file for output\n");
-    fclose(file);
-    return 1;
+    fclose(inputFile);
+    return ERROR;
   }
 
-  fprintf(outFile, "bits 16\n\n");
+  fprintf(outputFile, "bits 16\n\n");
 
-  int firstByte, secondByte;
+  int32_t firstByte, secondByte;
   char result[50];
-  while ((firstByte = fgetc(file)) != EOF && (secondByte = fgetc(file)) != EOF) {
-    decode(firstByte, secondByte, result, sizeof(result));
-    fprintf(outFile, "%s\n", result);
+
+  while (true) {
+    firstByte = fgetc(inputFile);
+    secondByte = fgetc(inputFile);
+    if ((secondByte == EOF) || (secondByte == 0)) {
+      break;
+    }
+
+    if(Decode(firstByte, secondByte, result, sizeof(result)) != OK) {
+      fprintf(stderr, "Error decoding opcode\n");
+      fclose(inputFile);
+      fclose(outputFile);
+      return ERROR;
+    }
+
+    fprintf(outputFile, "%s\n", result);
   }
 
-  fclose(file);
-  fclose(outFile);
+  fclose(inputFile);
+  fclose(outputFile);
 
   return 0;
 }
 
-void decode(uint8_t firstByte, uint8_t secondByte, char *result, size_t resultSize) {
-  MovInstruction movInstruction;
+void CopyOpcodeIntoResult(char *destination, uint8_t opcode) {
+  switch (opcode) {
+  case 0b100010: {
+    char opcodeString[] = "mov ";
+    strcpy_s(destination, strlen(opcodeString) + 1, opcodeString);
+  } break;
 
-  movInstruction.instruction = (firstByte >> 2) & 0b00111111;
-  movInstruction.d = (firstByte >> 1) & 0b00000001;
-  movInstruction.w = firstByte & 0b00000001;
-  movInstruction.mod = (secondByte >> 6) & 0b00000011;
-  movInstruction.reg = (secondByte >> 3) & 0b00000111;
-  movInstruction.rm = secondByte & 0b00000111;
-
-  if (movInstruction.instruction != 0b100010) {
-    strcpy_s(result, resultSize, "Invalid instruction");
-    return;
+  default:
+    printf("Invalid opcode\n");
   }
+}
 
-  strcpy_s(result, resultSize, "mov ");
+const char *sixteenBitRegisters[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+const char *eigthBitRegisters[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
 
-  const char *source;
-  const char *destination;
+void GetReg(uint8_t mod, uint8_t w, uint8_t reg, char *output) {
+  switch (mod) {
+  case 0b11: {
+    if (w == 1) {
+      strcpy_s(output, strlen(sixteenBitRegisters[reg]) + 1, sixteenBitRegisters[reg]);
+    } else {
+      strcpy_s(output, strlen(eigthBitRegisters[reg]) + 1, eigthBitRegisters[reg]);
+    }
+  } break;
 
-  if (movInstruction.d) {
-    destination = movInstruction.w ? registerFieldsWide[movInstruction.reg] : registerFieldsLow[movInstruction.reg];
-    source = movInstruction.w ? registerFieldsWide[movInstruction.rm] : registerFieldsLow[movInstruction.rm];
+  }
+}
+
+void GetRm(uint8_t mod, uint8_t w, uint8_t rm, char *output) {
+  switch (mod) {
+  case 0b11: {
+    if (w == 1) {
+      strcpy_s(output, strlen(sixteenBitRegisters[rm]) + 1, sixteenBitRegisters[rm]);
+    } else {
+      strcpy_s(output, strlen(eigthBitRegisters[rm]) + 1, eigthBitRegisters[rm]);
+    }
+  } break;
+
+  }
+}
+
+
+int32_t Decode(uint8_t firstByte, uint8_t secondByte, char *result, size_t maxSize)
+{
+  uint8_t opcode = firstByte >> 2;
+  uint8_t d = (firstByte >> 1) & 0b1;
+  uint8_t w = firstByte & 0b1;
+  uint8_t mod = (secondByte >> 6) & 0b11;
+  uint8_t reg = (secondByte >> 3) & 0b111;
+  uint8_t rm = secondByte & 0b111;
+
+  CopyOpcodeIntoResult(result, opcode);
+
+  char source[10];
+  char destination[10];
+
+  if (d == 1) {
+    // Register is the destination field
+    GetReg(mod, w, reg, destination);
+    GetRm(mod, w, rm, source);
   } else {
-    destination = movInstruction.w ? registerFieldsWide[movInstruction.rm] : registerFieldsLow[movInstruction.rm];
-    source = movInstruction.w ? registerFieldsWide[movInstruction.reg] : registerFieldsLow[movInstruction.reg];
+    // Register is the source field
+    GetReg(mod, w, reg, source);
+    GetRm(mod, w, rm, destination);
   }
 
-  strncat_s(result, resultSize, destination, resultSize - strlen(result) - 1);
-  strncat_s(result, resultSize, ", ", resultSize - strlen(result) - 1);
-  strncat_s(result, resultSize, source, resultSize - strlen(result) - 1);
+  strncat_s(result, maxSize, destination, maxSize - strlen(result) - 1);
+  strncat_s(result, maxSize, ", ", maxSize - strlen(result) - 1);
+  strncat_s(result, maxSize, source, maxSize - strlen(result) - 1);
+
+  return OK;
 }
 
