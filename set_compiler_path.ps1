858b516f11197fbf669d9f395e432110c074c8c6
Wicus <wicus@mecalc.co.za>
2024-01-20 12:07:21 +0200

    git cleanup

---
+++ set_compiler_path.ps1
@@ -0,0 +1,3 @@
+Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell -VsInstallPath "C:\Program Files\Microsoft Visual Studio\2022\Community" -DevCmdArguments '-arch=x64'
+cd $PSScriptRoot
