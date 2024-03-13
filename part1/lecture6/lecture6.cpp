1ca73a734e3475878df5a79b781b47528d640c49
Wicus <wicus@mecalc.co.za>
2024-03-13 09:08:46 +0200

    lecture 6 first part

--- part1/lecture6/lecture6.cpp
+++ part1/lecture6/lecture6.cpp
@@ -12,16 +12,103 @@
 
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <stdio.h>
-
-#define OK       0
-#define ERROR    1
-#define MAX_SIZE 100
 
 #include "sim86_shared.h"
 #pragma comment(lib, "sim86_shared_debug.lib")
 
-char *ReadFile(const char *filePath, size_t *size)
+#define OK                0
+#define ERROR             1
+#define MAX_SIZE          100
+#define ArrayCount(array) (sizeof(array) / sizeof(array[0]))
+
+typedef struct RegisterValue
+{
+    char reg[100];
+    int32_t value;
+} RegisterValue;
+
+static char *ReadFile(const char *filePath, size_t *size);
+static int32_t GetRegisterValue(const char *name, RegisterValue *registerValues, int32_t size);
+
+int32_t main(int32_t argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <filename>\n", argv[0]);
+        return ERROR;
+    }
+
+    size_t fileSize;
+    unsigned char *input = (unsigned char *)ReadFile(argv[1], &fileSize);
+    if (input == NULL)
+    {
+        printf("Error reading file: %s\n", argv[1]);
+        return ERROR;
+    }
+
+    instruction_table table;
+    Sim86_Get8086InstructionTable(&table);
+    printf("8086 Instruction Instruction Encoding Count: %u\n", table.EncodingCount);
+
+    FILE *outputFile;
+    errno_t err = fopen_s(&outputFile, "output.txt", "w");
+    if (err != 0)
+    {
+        printf("Error opening file for output\n");
+        return ERROR;
+    }
+
+    fprintf(outputFile, "bits 16\n\n");
+
+    RegisterValue registerValues[20] = {0};
+    int32_t offset = 0;
+    int32_t index = 0;
+    while (offset < fileSize)
+    {
+        instruction decodedInstruction;
+        unsigned char *source = &input[offset];
+        Sim86_Decode8086Instruction(fileSize - offset, source, &decodedInstruction);
+        if (!decodedInstruction.Op)
+        {
+            printf("Unrecognized instruction\n");
+            break;
+        }
+
+        offset += decodedInstruction.Size;
+
+        RegisterValue registerValue = {0};
+        strncpy(registerValue.reg, Sim86_RegisterNameFromOperand(&decodedInstruction.Operands[0].Register), 100);
+        int32_t currentRegisterValue = GetRegisterValue(registerValue.reg, registerValues, ArrayCount(registerValues));
+        registerValue.value = decodedInstruction.Operands[1].Immediate.Value;
+
+        registerValues[index++] = registerValue;
+
+        fprintf(outputFile, "%s ", Sim86_MnemonicFromOperationType(decodedInstruction.Op));
+        fprintf(outputFile, "%s, ", registerValue.reg);
+        fprintf(outputFile, "%d; ", registerValue.value);
+        fprintf(outputFile, "%s: 0x%x -> ", registerValue.reg, currentRegisterValue);
+        fprintf(outputFile, "0x%x\n", registerValue.value);
+    }
+
+    fprintf(outputFile, "\nFinal Registers\n\n");
+
+    for (int i = 0; i < ArrayCount(registerValues); i++)
+    {
+        if (registerValues[i].reg[0] == '\0')
+        {
+            break;
+        }
+
+        fprintf(outputFile, "    %s: ", registerValues[i].reg);
+        fprintf(outputFile, "0x%04x (%d)\n", registerValues[i].value, registerValues[i].value);
+    }
+
+    return 0;
+}
+
+static char *ReadFile(const char *filePath, size_t *size)
 {
     // Open the file
     FILE *file = NULL;
@@ -66,66 +153,14 @@ char *ReadFile(const char *filePath, size_t *size)
     return buffer;
 }
 
-typedef struct RegisterValue
+static int32_t GetRegisterValue(const char *name, RegisterValue *registerValues, int32_t size)
 {
-    const char name[100];
-    int32_t value;
-} RegisterValue;
-
-int32_t main(int32_t argc, char *argv[])
-{
-    if (argc < 2)
+    for (int i = 0; i < size; i++)
     {
-        printf("Usage: %s <filename>\n", argv[0]);
-        return ERROR;
-    }
-
-    size_t fileSize;
-    unsigned char *input = (unsigned char *)ReadFile(argv[1], &fileSize);
-    if (input == NULL)
-    {
-        printf("Error reading file: %s\n", argv[1]);
-        return ERROR;
-    }
-
-    instruction_table table;
-    Sim86_Get8086InstructionTable(&table);
-    printf("8086 Instruction Instruction Encoding Count: %u\n", table.EncodingCount);
-
-    FILE *outputFile;
-    errno_t err = fopen_s(&outputFile, "output.asm", "w");
-    if (err != 0)
-    {
-        printf("Error opening file for output\n");
-        return ERROR;
-    }
-
-    fprintf(outputFile, "bits 16\n\n");
-
-    RegisterValue *registerValues;
-    int32_t offset = 0;
-    while (offset < fileSize)
-    {
-        instruction decodedInstruction;
-        unsigned char *source = &input[offset];
-        Sim86_Decode8086Instruction(fileSize - offset, source, &decodedInstruction);
-        if (!decodedInstruction.Op)
+        if (strncmp(name, registerValues[i].reg, 100) == 0)
         {
-            printf("Unrecognized instruction\n");
-            break;
+            return registerValues[i].value;
         }
-
-        offset += decodedInstruction.Size;
-
-        RegisterValue registerValue;
-        registerValue.name = Sim86_RegisterNameFromOperand(&decodedInstruction.Operands[0].Register);
-        registerValue.value = decodedInstruction.Operands[1].Immediate.Value;
-        registerValues[0] = registerValue;
-
-        fprintf(outputFile, "%s ", Sim86_MnemonicFromOperationType(decodedInstruction.Op));
-        fprintf(outputFile, "%s, ", registerValue.name);
-        fprintf(outputFile, "%d ; ", registerValue.value);
-        fprintf(outputFile, "%s:0x0->0x%x\n", registerName, re
     }
 
     return 0;
