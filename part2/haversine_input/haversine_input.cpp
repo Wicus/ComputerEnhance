e8e38f713617b32da53cca98bb58038dd1d4f397
Wicus <wicus@mecalc.co.za>
2024-06-24 09:27:47 +0200

    WIP

--- part2/haversine_input/haversine_input.cpp
+++ part2/haversine_input/haversine_input.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <cstdio>
 #include <fstream>
 #include <iostream>
 #include <memory>
@@ -12,6 +14,8 @@ struct Pair
     int y1;
 };
 
+const int numberOfRegions = 64;
+
 struct JsonFile
 {
     std::string path;
@@ -20,6 +24,28 @@ struct JsonFile
 
     JsonFile(std::string path) : path(path)
     {}
+
+    void AddRandom(int number)
+    {
+        int numberOf = 0;
+        for (int i = numberOfRegions; i > 0; i--)
+        {
+            int lonRange = 360;
+            int latRange = 180;
+            for (int j = 0; j < number / numberOfRegions; j++)
+            {
+                int x0 = (rand() % 361 - 180) / i;
+                int x1 = (rand() % 361 - 180) / i;
+                int y0 = (rand() % 181 - 90) / i;
+                int y1 = (rand() % 181 - 90) / i;
+                pairs->push_back({x0, y0, x1, y1});
+                numberOf++;
+            }
+
+        }
+
+        printf("%d\n", numberOf);
+    }
 
     void Write()
     {
@@ -35,12 +61,8 @@ struct JsonFile
 
         for (size_t i = 0; i < pairs->size(); i++)
         {
-            file << "        {\n";
-            file << "            \"x0\": " << pairs->at(i).x0 << std::endl;
-            file << "            \"y0\": " << pairs->at(i).y0 << std::endl;
-            file << "            \"x1\": " << pairs->at(i).x1 << std::endl;
-            file << "            \"y1\": " << pairs->at(i).y1 << std::endl;
-            file << "        }";
+            file << "        { \"x0\": " << pairs->at(i).x0 << ", \"y0\": " << pairs->at(i).y0
+                 << ", \"x1\": " << pairs->at(i).x1 << ", \"y1\": " << pairs->at(i).y1 << " }";
 
             if (i < pairs->size() - 1)
             {
@@ -61,9 +83,7 @@ int main(int argc, char *argv[])
 {
     JsonFile jsonFile("haversine_input.json");
 
-    jsonFile.pairs->push_back({0, 0, 1, 1});
-    jsonFile.pairs->push_back({1, 1, 2, 2});
-
+    jsonFile.AddRandom(1000);
     jsonFile.Write();
 
     return 0;
