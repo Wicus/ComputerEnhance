8525c02609c967cd81574fbfd53b914e4365a3b3
Wicus Pretorius <wicus.pretorius@iotnxt.com>
2025-04-29 05:17:06 +0200

    fix: random generator

--- HaversineInputJson/PairGenerator.cs
+++ HaversineInputJson/PairGenerator.cs
@@ -19,47 +19,25 @@ public class HaversineGenerator
     {
         var pairs = new List<Pair>();
         var random = new Random(seed);
+        var maxAllowedX = 180;
+        var maxAllowedY = 90;
 
-        int fullBlocks = numberOfPairs / 64;
-        int remainder = numberOfPairs % 64;
-
-        for (int i = 0; i < fullBlocks; i++)
+        for (var i = 0; i < numberOfPairs; i += 64)
         {
-            for (int j = 0; j < 64; j++)
+            var xCenter = Math.Clamp(GetRandomX(random), -maxAllowedX, maxAllowedX);
+            var yCenter = Math.Clamp(GetRandomY(random), -maxAllowedY, maxAllowedY);
+            var xRadius = Math.Clamp(GetRandomX(random), 0, maxAllowedX);
+            var yRadius = Math.Clamp(GetRandomY(random), 0, maxAllowedY);
+
+            for (var j = 0; j < 64 && i + j < numberOfPairs; j++)
             {
-                var x0 = random.NextDouble() * 360 - 180;
-                var x1 = random.NextDouble() * 360 - 180;
-                var y0 = random.NextDouble() * 180 - 90;
-                var y1 = random.NextDouble() * 180 - 90;
+                var x0 = RandomDegree(random, xCenter, xRadius, maxAllowedX);
+                var x1 = RandomDegree(random, xCenter, xRadius, maxAllowedX);
+                var y0 = RandomDegree(random, yCenter, yRadius, maxAllowedY);
+                var y1 = RandomDegree(random, yCenter, yRadius, maxAllowedY);
 
-                pairs.Add(
-                    new Pair
-                    {
-                        x0 = x0,
-                        x1 = x1,
-                        y0 = y0,
-                        y1 = y1,
-                    }
-                );
+                pairs.Add(new Pair { x0 = x0, x1 = x1, y0 = y0, y1 = y1 });
             }
-        }
-
-        for (int j = 0; j < remainder; j++)
-        {
-            var x0 = random.NextDouble() * 360 - 180;
-            var x1 = random.NextDouble() * 360 - 180;
-            var y0 = random.NextDouble() * 180 - 90;
-            var y1 = random.NextDouble() * 180 - 90;
-
-            pairs.Add(
-                new Pair
-                {
-                    x0 = x0,
-                    x1 = x1,
-                    y0 = y0,
-                    y1 = y1,
-                }
-            );
         }
 
         return pairs;
@@ -77,13 +55,7 @@ public class HaversineGenerator
         return sum;
     }
 
-    private static double ReferenceHaversine(
-        double x0,
-        double y0,
-        double x1,
-        double y1,
-        double earthRadius
-    )
+    private static double ReferenceHaversine(double x0, double y0, double x1, double y1, double earthRadius)
     {
         var lat1 = y0;
         var lat2 = y1;
@@ -95,9 +67,7 @@ public class HaversineGenerator
         lat1 = RadiansFromDegrees(lat1);
         lat2 = RadiansFromDegrees(lat2);
 
-        var a =
-            Square(Math.Sin(dLat / 2.0))
-            + Math.Cos(lat1) * Math.Cos(lat2) * Square(Math.Sin(dLon / 2));
+        var a = Square(Math.Sin(dLat / 2.0)) + Math.Cos(lat1) * Math.Cos(lat2) * Square(Math.Sin(dLon / 2));
         var c = 2.0 * Math.Asin(Math.Sqrt(a));
 
         var result = earthRadius * c;
@@ -114,6 +84,26 @@ public class HaversineGenerator
     private static double RadiansFromDegrees(double degrees)
     {
         double result = 0.01745329251994329577f * degrees;
+        return result;
+    }
+
+    private static double GetRandomX(Random random)
+    {
+        return random.NextDouble() * 360 - 180;
+    }
+
+    private static double GetRandomY(Random random)
+    {
+        return random.NextDouble() * 180 - 90;
+    }
+
+    private static double RandomDegree(Random random, double center, double radius, double maxAllowed)
+    {
+        var minVal = Math.Max(center - radius, -maxAllowed);
+        var maxVal = Math.Min(center + radius, maxAllowed);
+
+        var range = maxVal - minVal;
+        var result = minVal + random.NextDouble() * range;
         return result;
     }
 }
